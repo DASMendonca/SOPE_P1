c4edfe66612f24af5269138da0cdc2cde291e67b
Daniel Mendonca <ei12167@fe.up.pt>
2014-04-04 22:04:45 +0100

    first commit

---
+++ src/proj1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : proj1.c
+ Author      : Daniel Mendonça
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Projecto 1 de Sistemas Operativos"); /* prints Projecto 1 de Sistemas Operativos */
+	return EXIT_SUCCESS;
+}
