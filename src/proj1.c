b60c89ec32b024ec1a5076a6876c1a8d5abf87fc
Daniel Mendonca <ei12167@fe.up.pt>
2014-04-07 02:01:26 +0100

    improvements

--- src/proj1.c
+++ src/proj1.c
@@ -10,6 +10,10 @@
 
 #include "proj1.h"
 
+#define TESTING 1
+#define READ 0
+#define WRITE 1
+
 int main(int argc, char *argv[]) {
 
 	int run_time;
@@ -20,13 +24,15 @@ int main(int argc, char *argv[]) {
 		puts("Invalid input.\nInput example: ./monitor time word filename1 filename2 ... filenameN\n");
 		return -1;
 	}
+	//printf("\n"); //testing
 
 	run_time = atoi(argv[1]);
-	printf("run_time = %d\n", run_time);  //for testing
+	//printf("run_time = %d\n", run_time);  //testing
 
 	int i = 3;
 	for(; i<argc; i++){
 		int tmp = i -3;
+		printf("\nprocess nr: %d\n", tmp);
 
 		pid = fork();
 		if(pid == 0){
@@ -35,18 +41,17 @@ int main(int argc, char *argv[]) {
 			return 0;
 		}
 		else{
-			//File existence verification
 			__pid_t tmp_pid = pid;
 			pgr_id[tmp]= pid;
-			setpgid(pid, 0);
+			setpgid(pid, 0); //child pgrid is the sames as his pid
 
-
+			//creating monitor_aux for the file and process
 			if((pid = fork())==0){
 				monitorAux(argv[i]);
 				return 0;
 			}
 			else{
-				setpgid(pid, tmp_pid);
+				setpgid(pid, tmp_pid);	//monitor_aux pgrid to the same of monitor_word
 				pgr_id[tmp]= tmp_pid;
 			}
 		}
@@ -62,25 +67,26 @@ int main(int argc, char *argv[]) {
 /*
  * check if files still exist every 5sec
  */
-void monitorAux(char *file_name){
+void monitorAux(char *filename){
 	//TODO
-	sleep(1);	//just for testing
-	printf("I'm monitor_aux, my pid is: %d\nMy pgid is: %d\n", (int) getpid(), (int) getpgrp()); //testing
+	sleep(1);
+	printf("I'm a monitor_aux, my pid is: %d and my pgid is: %d\n", (int) getpid(), (int) getpgrp()); //testing
+	printf("%d: I'm watching %s\n", (int) getpid(), filename); //testing
 	int fd;
 	while(1){
 		sleep(5);
-		fd = open(file_name, O_RDONLY);
+		fd = open(filename, O_RDONLY);
 		if(fd== -1)
 			break;
 		else
 			close(fd);
 	}
-	printf("File %s does not exist and will not be tracked\n", file_name);
+	printf("File %s does not exist and will not be tracked\n", filename); //testing
 	kill((-1 * getpgrp()), SIGUSR1);
 }
 
 /**
- * playing with dup2, pipes and exec
+ * using with dup2, pipes and exec
  *
  */
 void monitorWord(char *word, char *filename){
@@ -88,6 +94,10 @@ void monitorWord(char *word, char *filename){
 	sleep(60); //just for testing
 }
 
+
+/*
+ * Ending program.
+ */
 void killAll(__pid_t pgids[], size_t size){
 
 }
