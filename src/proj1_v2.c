613008b28d990c976503816a329dd522b8fa0f45
Daniel Mendonca <ei12167@fe.up.pt>
2014-04-10 04:13:47 +0100

    version 2, with waitpid

---
+++ src/proj1_v2.c
@@ -0,0 +1,263 @@
+/*
+ ============================================================================
+ Name        : proj1.c
+ Author      : Daniel Mendonça
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Project 1, SOPE
+ ============================================================================
+ */
+
+#include "proj1.h"
+
+__pid_t *pgrids;
+
+/*
+ * handle terminated children and classifies the process group as dead, so the parent don't try to kill it again
+ */
+void sig_handler(int signo)
+{
+	int stat;
+	__pid_t pid;	//v2
+	pid = waitpid(-1, &stat, WNOHANG);
+
+	if(DEBUG)
+	{
+		printf("sig_handler of pid:%d\n", getpid());
+		printf("process killed: %d\n", pid);
+	}
+
+	doNotFollow(pid, pgrids);
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+
+	int runtime, alive_procs=(argc-3);
+	__pid_t pid;
+
+	struct sigaction action;
+	action.sa_handler = sig_handler;
+	sigemptyset(&action.sa_mask);
+	//action.sa_flags = SA_NOCLDWAIT; //v1
+	action.sa_flags = 0; //v2
+
+	if (sigaction(SIGCHLD, &action, NULL) < 0)
+	{
+		fprintf(stderr,"Unable to install SIGNAL handler\n");
+		exit(1);
+	}
+	pgrids=malloc(sizeof(__pid_t) * alive_procs);
+
+
+	if(argc<4)
+	{
+		puts("Invalid input.\nInput example: ./monitor time word filename1 filename2 ... filenameN\n");
+		return -1;
+	}
+
+	runtime = atoi(argv[1]);
+	int i = 3;
+	for(; i<argc; i++)
+	{
+		int tmp = i -3;
+
+		if((pid= fork()) == 0)
+		{
+			//word control
+			monitorWord(argv[2], argv[i]);
+			return 0;
+		}
+		else
+		{
+			pgrids[tmp]= pid;
+			setpgid(pid, pid); //child pgrid is the same as his pid
+		}
+
+	}
+
+
+
+	//creating monitor_aux for the file and process
+	if((pid = fork())==0)
+	{
+		monitorExistence(argv, pgrids, argc);
+		return 0;
+	}
+
+	while(( runtime = sleep(runtime)) != 0)
+	{	//v2
+		alive_procs--;
+		if(alive_procs<=0)
+			break;
+	}
+	killAll(pgrids, &alive_procs, pid); //v2
+	return 0;
+}
+
+
+/*
+ * check if files being monitored still exist every 5sec
+ * filenames given from argv[3] to argv[argc-1]
+ * if the file does not exist anymore, the process group monitoring its words is killed
+ */
+void monitorExistence(char *argv[], __pid_t *pgrid, int argc){
+	sleep(TIME_BEFORE_LAUNCH);
+
+	if(DEBUG)
+		printf("I am the existence monitor. My pid: %d. My ppid: %d\n", getpid(), getppid());
+
+	int fd;
+	int i;
+	int kill_return;
+	while(1){
+		sleep(5);
+		i=3;
+		for(; i<argc; i++)	//trying to open the files
+		{
+			int tmp = i-3;	//tmp is the matching pid index at pgrid
+			if(pgrid[tmp] != 0)
+			{
+
+				fd = open(argv[i], O_RDONLY);
+				if(fd== -1)
+				{
+					printf("File %s does not exist and will not be tracked\n", argv[i]);
+					if((kill_return=kill(-pgrid[tmp], SIGUSR1))== -1)
+						printf("%s\n", strerror(errno));
+					pgrid[tmp]=0;
+				}
+				else{
+					close(fd);
+					if(DEBUG)
+						printf("Verified existence of %s, that belongs with %d group\n", argv[i], pgrid[tmp]);
+				}
+			}
+
+		}
+	}
+}
+
+
+/**
+ * Creates 2 children, one running tail and  one grep, connecting them with a pipe, and connects itself
+ * with grep, so it can catch every phrase with the word you want to find in the file filename.
+ */
+void monitorWord(char *word, char *filename){
+	sleep(TIME_BEFORE_LAUNCH);
+	int pipe1[2], pipe2[2];
+	pipe(pipe1);
+	__pid_t pid;
+
+	if((pid=fork())==0)
+	{
+		if(DEBUG)
+			whoAmI(filename, "tail");
+		close(pipe1[READ]);
+		dup2(pipe1[WRITE], STDOUT_FILENO);
+		execlp("tail", "tail", "-fn 1", filename, NULL);
+	}
+	else
+	{
+		setpgid(pid, getpid());
+		pipe(pipe2);
+
+		if((pid=fork())== 0)
+		{
+			if(DEBUG)
+				whoAmI(filename, "grep");
+			close(pipe1[WRITE]);
+			close(pipe2[READ]);
+			dup2(pipe1[READ], STDIN_FILENO);
+			dup2(pipe2[WRITE], STDOUT_FILENO);
+			execlp("grep", "grep", "--line-buffered", word, NULL);
+		}
+		else
+		{
+			if(DEBUG)
+				whoAmI(filename, "communication");
+			setpgid(pid, getpid());
+			close(pipe2[WRITE]);
+
+			time_t t = time(NULL);
+			struct tm tm = *localtime(&t);
+			char receive[MAX_SIZE];
+			int read_bytes= 0;
+
+			while((read_bytes= read(pipe2[READ], receive, MAX_SIZE))!= -1 || read_bytes!= 0)
+			{
+				t= time(NULL);
+				tm = *localtime(&t);
+				receive[read_bytes]='\0';
+				//tm_mon january is 0 december is 11
+				printf("%4d-%02d-%02dT%02d:%02d:%02d - %s - %s", tm.tm_year + 1900, tm.tm_mon+1, tm.tm_mday,
+						tm.tm_hour, tm.tm_min, tm.tm_sec, filename, receive);
+			}
+		}
+	}
+}
+
+
+/*
+ * Kills all processes
+ */
+void killAll(__pid_t *pgids, int *alive_procs, __pid_t file_checker){
+	int i=0;
+	int kill_return;
+	if(DEBUG)
+		printf("This is killAll\n");
+	for(; i< (*alive_procs); i++)
+	{
+		if(DEBUG)
+			printf("pgid[%d]: %d\n", i, pgids[i]);
+		if(pgids[i]!= 0)
+			if((kill_return=kill((-1*pgids[i]), SIGUSR1)) == -1)
+				printf("error killing group %d: %s\n", pgids[i], strerror(errno));
+
+	}
+	if((kill_return=kill(file_checker, SIGUSR1)) == -1)
+		printf("error killing %d: %s\n", file_checker, strerror(errno));
+}
+
+
+/*
+ * For Debug purposes only.
+ */
+void whoAmI(char *filename, char *whatIDo){
+	printf("I'm %d, son of %d, of the group %d. I do %s of %s\n",getpid(), getppid(), getpgrp(), whatIDo, filename);
+}
+
+
+/*
+ * sets to 0 matching pid in the array of pids, so we can avoid trying to kill an inexistent process
+ */
+void doNotFollow(__pid_t pgrid, __pid_t *pgrids){
+	int i= 0;
+	//printf("pgr to kill: %d, pgrids:%d\n", pgrid, pgrids[0]);
+	while(pgrids[i] != pgrid){
+		i++;
+	}
+	pgrids[i]=0;
+	if(DEBUG)
+		printf("i= %d: pgrid removed: %d\n", i, pgrids[i]);
+}
+
+
+/*
+ * Try to open with readonly permission the filename. Return -1 if it fails.
+ */
+int checkFileExistence(char *filename){
+	int fd;
+
+
+	return 0;
+}
+
+
+//kill a process group
+void killOneProcessGroup(__pid_t pgid){
+	//TODO
+}
+
---
+++ src/proj1_v2.h
@@ -0,0 +1,58 @@
+/*
+ * proj1.h
+ *
+ *  Created on: Apr 5, 2014
+ *      Author: daniel
+ */
+
+#ifndef PROJ1_H_
+#define PROJ1_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <execinfo.h>
+#include <time.h>
+#include <sys/wait.h>
+
+
+#define __USE_GNU
+#include <ucontext.h>
+
+#define DEBUG 1
+#define READ 0
+#define WRITE 1
+#define MAX_SIZE 2048
+#define TIME_BEFORE_LAUNCH 2
+
+
+
+void intHandler(int sign);
+
+void sigusr1Handler();
+
+void monitorExistence(char *filenames[], __pid_t *pgrid, int files_nr);
+
+void monitorWord(char *word, char *filename);
+
+void killAll(__pid_t *pgids, int *size, __pid_t file_checker);
+
+void waitForChildren(__pid_t *pgids, int status, size_t size);
+
+void whoAmI(char *filename, char *whatIDo);
+
+void sig_handler(int signo);
+
+void doNotFollow(__pid_t pgrid, __pid_t *pgrids);
+
+int checkFileExistence(char *filename);
+
+void killOneProcessGroup(__pid_t pgid);
+
+#endif /* PROJ1_H_ */
